14fe50a3640bfc7a03ee0c76d60e5ab94f0b1dd5
Leandro Rufino <leandrix1234@gmail.com>
2024-12-11 00:34:43 -0300

    handling fetch time

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetchingData, setIsFetchingData] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetchingData(true);
       const response = await fetch("http://localhost:3000/places");
       const responseData = await response.json();
       setAvailablePlaces(responseData.places);
+      setIsFetchingData(false);
     }
     fetchPlaces();
   }, []);
@@ -17,6 +20,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetchingData}
+      isLoadingText={"Fetching places..."}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
